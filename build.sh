604cf0ab9751017c35201f8da80b61cc20d51845
Gurvinder Singh <gmpire.developer@gmail.com>
2026-01-12 10:59:01 +0530

    Prepare for Vercel deployment

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # Build the project
 echo "Building the project..."
-python3.12.5 -m pip install -r requirements.txt
+python3.11 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.12.5 manage.py makemigrations --noinput
-python3.12.5 manage.py migrate --noinput
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
 echo "Collect Static..."
-python3.12.5 manage.py collectstatic --noinput --clear
+python3.11 manage.py collectstatic --noinput --clear
